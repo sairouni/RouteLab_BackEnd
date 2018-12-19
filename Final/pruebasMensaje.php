fd184e9f44677a847383780cd0a03ba5b5b8b1d1
marcfv11 <marcfv11@gmail.com>
2018-12-19 12:15:26 +0100

    Arreglada la clase Mensajeria, ahora se puede usar el chat de mensajes almacenando los mensajes en la base de datos.

---
+++ Final/pruebasMensaje.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        require_once 'classMensajeria.php';
+        require_once 'usuario.php';
+
+        $b = new Mensajeria();
+        $b->fecha = "2018-12-13";
+        $b->hora = "10:30";
+        $d = new Usuario();
+        $d->load(1);
+        $b->emisor = $d;
+        $j = new Usuario();
+        $j->load(2);
+        $b->receptor = $j;
+        $b->mensaje="Send Nudes Pls";
+        print_r($d);
+        print_r($j);
+        $b->save();
+        ?>
+    </body>
+</html>
