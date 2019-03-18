d1564ebc82362d6b8fbca57ec74fa61335d0a8dd
marcfv11 <marcfv11@gmail.com>
2019-03-18 12:38:43 +0100

    Funcion de obtener recomendaciones. Obtener todas las recomendaciones por una sola descripcion.

--- Final/Clases/Recomendaciones.php
+++ Final/Clases/Recomendaciones.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once 'BasedeDatos.php';
 
 /*
@@ -14,110 +15,111 @@ require_once 'BasedeDatos.php';
  */
 class Recomendaciones extends BasedeDatos {
 
-   //Las propiedades mapean las existentes en la base de datos
-   private $idrec;
-   private $icono;
-   private $descripcion;
-   private $num_fields=3;
+    //Las propiedades mapean las existentes en la base de datos
+    private $idrec;
+    private $icono;
+    private $descripcion;
+    private $pclave;
+    private $num_fields = 4;
 
+    function __construct() {
+        $show = ["icono"];
+        $fields = array_slice(array_keys(get_object_vars($this)), 0, $this->num_fields);
+        parent::__construct("recomendaciones", "idrec", $fields, $show);
+    }
 
+    function getidRec() {
+        return $this->idrec;
+    }
 
+    function getIcono() {
 
-   function __construct() {
-       $show = ["icono"];
-       $fields = array_slice(array_keys(get_object_vars($this)), 0, $this->num_fields);
-       parent::__construct("recomendaciones", "idrec", $fields, $show);
-   }
+        return $this->icono;
+    }
 
+    function getDescripcion() {
+        return $this->descripcion;
+    }
+    
+    function getPclave() {
+        return $this->pclave;
+    }
 
+    function setIcono($icono) {
+        $this->icono = $icono;
+    }
 
+    function setDescripcion($descripcion) {
+        $this->descripcion = $descripcion;
+    }
+    
+    function setPclave($pclave) {
+        $this->pclave = $pclave;
+    }
 
+    function __get($name) {
+        $metodo = "get$name";
+        if (method_exists($this, $metodo)) {
+            return $this->$metodo();
+        } else {
+            throw new Exception("Propiedad no encontrada");
+        }
+    }
 
-function getidRec() {
-       return $this->idrec;
-   }
+    function __set($name, $value) {
+        $metodo = "set$name";
+        if (method_exists($this, $metodo)) {
+            return $this->$metodo($value);
+        } else {
+            throw new Exception("Propiedad no encontrada");
+        }
+    }
 
-   function getIcono() {
+    function load($id) {
+        $recomendaciones = $this->getById($id);
 
-       return $this->icono;
-   }
+        if (!empty($recomendaciones)) {
+            $this->idrec = $id;
+            $this->icono = $recomendaciones['icono'];
+            $this->descripcion = $recomendaciones['descripcion'];
+            $this->pclave = $recomendaciones['pclave'];
+        } else {
+            throw new Exception("No existe ese registro");
+        }
+    }
 
-   function getDescripcion() {
-       return $this->descripcion;
-   }
+    function delete() {
+        if (!empty($this->idrec)) {
+            $this->deleteById($this->idrec);
+            $this->idrec = null;
+            $this->icono = null;
+            $this->descripcion = null;
+            $this->pclave = null;
+        } else {
+            throw new Exception("No hay registro para borrar");
+        }
+    }
 
+    function save() {
+        $recomendaciones = $this->valores();
+        unset($recomendaciones['idrec']);
+        if (empty($this->idrec)) {
+            $this->insert($recomendaciones);
+            $this->idrec = self::$conn->lastInsertId();
+        } else {
+            $this->update($this->idrec, $recomendaciones);
+        }
+    }
 
-      function setIcono($icono) {
-       $this->icono = $icono;
-   }
+    public function getbyRec($id) {
+        $user = $this->getAll(['pclave' => $id]);
+        if (!empty($user)) {
+            return $user;
+        } else {
+            throw new Exception("No existe ese registro");
+        }
+    }
 
-   function setDescripcion($descripcion) {
-       $this->descripcion = $descripcion;
-   }
-
-
-
-
-
-function __get($name) {
-       $metodo = "get$name";
-       if (method_exists($this, $metodo)) {
-           return $this->$metodo();
-       } else {
-           throw new Exception("Propiedad no encontrada");
-       }
-   }
-
-   function __set($name, $value) {
-       $metodo = "set$name";
-       if (method_exists($this, $metodo)) {
-           return $this->$metodo($value);
-       } else {
-           throw new Exception("Propiedad no encontrada");
-       }
-   }
-
-
-
-
-function load($id) {
-       $recomendaciones = $this->getById($id);
-
-       if (!empty($recomendaciones)) {
-           $this->idrec = $id;
-           $this->icono = $recomendaciones['icono'];
-           $this->descripcion = $recomendaciones['descripcion'];
-       } else {
-           throw new Exception("No existe ese registro");
-       }
-   }
-
-
-
-
-
-function delete() {
-       if (!empty($this->idrec)) {
-           $this->deleteById($this->idrec);
-           $this->idrec = null;
-           $this->icono = null;
-           $this->descripcion = null;
-       } else {
-           throw new Exception("No hay registro para borrar");
-       }
-   }
-
-
-   function save() {
-       $recomendaciones = $this->valores();
-       unset($recomendaciones['idrec']);
-       if (empty($this->idrec)) {
-           $this->insert($recomendaciones);
-           $this->idrec = self::$conn->lastInsertId();
-       } else {
-           $this->update($this->idrec, $recomendaciones);
-       }
-   }
 }
 
 //$a = new Alumno();
--- Final/Clases/WebService/Response.php
+++ Final/Clases/WebService/Response.php
@@ -6,7 +6,7 @@ public $data;
 function __construct($message, $data = "") {
 $this->message = $message;
 
-$this->data = (String)$data;
+$this->data = $data;
 }
 function __toString() {
 return json_encode($this);
