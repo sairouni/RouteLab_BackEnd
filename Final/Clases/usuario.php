6cdc74dae9cb335ed94d577c9600d0d44f15a257
sairouni <ismaelsairouni@gmail.com>
2019-03-20 11:56:17 +0100

    cambiamos los nombres de las funciones,y cambiamos en BD las nuevas credenciales

--- Final/Clases/usuario.php
+++ Final/Clases/usuario.php
@@ -1,7 +1,6 @@
 <?php
 require_once 'BasedeDatos.php';
 require_once 'Localidad.php';
-require_once 'valoracionUsu.php';
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
